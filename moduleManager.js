8e47576694fc0f1986c95c0bf2da2b64e5031f28
hwclass <harderworking.class@gmail.com>
2015-02-05 01:29:48 +0200

    Edited indentation and added use strict.

--- moduleManager.js
+++ moduleManager.js
@@ -1,19 +1,21 @@
+'use strict';
+
 var appName = (function ModuleManager(app) {
 
-    function define(name, deps, impl) {
-        for (var i=0; i<deps.length; i++) {
-            deps[i] = app.modules[deps[i]];
-        }
-        app.modules[name] = impl.apply(impl, deps);
-    }
-
-    function get(name) {
-        return app.modules[name];
-    }
-
-    return {
-        define: define,
-        get: get
+  function define(name, deps, impl) {
+    for (var i=0; i<deps.length; i++) {
+      deps[i] = app.modules[deps[i]];
     };
+    app.modules[name] = impl.apply(impl, deps);
+  };
+
+  function get(name) {
+    return app.modules[name];
+  };
+
+  return {
+    define: define,
+    get: get
+  };
 	
 })(app);
